130105c382feadaaac165e6572ce092705bb9120
An Phan <antphan98@gmail.com>
2020-02-13 10:35:39 -0600

    added html to index.handlebars

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 
-const router = express.Router();
+module.exports = function (app) {
 
-router.get("/", (req, res) => {
+app.get("/", (req, res) => {
   res.render("index");
 });
 
-router.get("/saved", (req, res) => {
+app.get("/saved", (req, res) => {
 	res.render("saved");
 });
 
-module.exports = router;
+
+};
